909ede34ee35b6e9d1c80835a3f5031daf37e0ef
Lucas Lima <robertolucas011@gmail.com>
2021-12-20 19:52:45 -0300

    Adiciona 7 e 8.

--- script.js
+++ script.js
@@ -3,15 +3,24 @@ const pegaInput = document.querySelector('#texto-tarefa');
 const pegaLista = document.querySelector('#lista-tarefas');
 
 function adicionaTarefa() {
-    console.log('ok');
   const li = document.createElement('li');
   li.innerText = pegaInput.value;
   pegaLista.appendChild(li);
   pegaInput.value = '';
-}
-
+  li.addEventListener('click', mudaBackgroundLi);
+  li.style.backgroundColor = 'white';
+  //li.classList.add('selected');
+  }
+  function mudaBackgroundLi(event){
+    const selecionaLi = document.querySelectorAll('li');
+    for (let index = 0; index < selecionaLi.length; index += 1) {
+      selecionaLi[index].classList.remove('selected');
+      selecionaLi[index].style.backgroundColor = 'white';
+    }
+    event.target.classList.add('selected');
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
 pegaBotao.addEventListener('click', adicionaTarefa);
-
 // pegaBotao.addEventListener('click', function () {
 //   const li = document.createElement('li');
 //   pegaLista.appendChild(li);
