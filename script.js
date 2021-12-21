7f02c51a4e6cd77c911d28b1b276d4857634e7cf
Lucas Lima <robertolucas011@gmail.com>
2021-12-20 21:29:48 -0300

    Adiciona requisito 9.

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function adicionaTarefa() {
   pegaInput.value = '';
   li.addEventListener('click', mudaBackgroundLi);
   li.style.backgroundColor = 'white';
+  li.addEventListener('dblclick', riscaLi);
+
   //li.classList.add('selected');
   }
   function mudaBackgroundLi(event){
@@ -20,7 +22,29 @@ function adicionaTarefa() {
     event.target.classList.add('selected');
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   }
+
+  function riscaLi(event){
+    const selecionaLi2 = document.querySelectorAll('li');
+    event.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+    event.target.classList.add('completed');
+    for (index = 0; index < selecionaLi2.length; index += 1){
+      selecionaLi2[index].addEventListener('dblclick', tiraRiscoLi)
+      ;
+    }
+  function tiraRiscoLi(event){
+    if (event.target.style.textDecoration === 'line-through solid rgb(0, 0, 0)') {
+      event.target.classList.remove('completed');
+      event.target.style.textDecoration = 'none';
+    } else {
+      event.target.classList.add('completed');
+      event.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+    }
+    }
+  }
 pegaBotao.addEventListener('click', adicionaTarefa);
+
+
+
 // pegaBotao.addEventListener('click', function () {
 //   const li = document.createElement('li');
 //   pegaLista.appendChild(li);
